bd5af10b35d6a7fefc1f4f0b85ccbf6ef8e13e32
Anthony Kozar <akozar@users.sourceforge.net>
2025-06-22 19:17:02 -0400

    main.py can accept a filename to open

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from aliba_parser import Lark_StandAlone
 from ParseTreeToProgram import ParseTreeToProgram
 
-f = open("examples/looping-counter.aliba", 'r')
+from sys import argv
+
+if len(argv) < 1:
+    f = open("examples/looping-counter.aliba", 'r')
+else:
+    f = open(argv[1], 'r')
 progtxt = f.read()
-f.close
+f.close()
 
 parser = Lark_StandAlone(transformer=ParseTreeToProgram())
 program = parser.parse(progtxt)
